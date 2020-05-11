2a8111854055f2f460e190e6a62adeb018aff546
FauzanAr <faurinta12345@gmail.com>
2020-05-11 13:44:31 +0700

    creating login and signup page

--- ui-todo/src/App.js
+++ ui-todo/src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import login from './pages/login';
+import signup from './pages/signup';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path='/login' component={login}/>
+            <Route exact path='/signup' component={signup}/>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
