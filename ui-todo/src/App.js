b670dcac48f112cc01a38894cef8928bf3b390ed
FauzanAr <faurinta12345@gmail.com>
2020-05-12 18:18:44 +0700

    finishing todo component

--- ui-todo/src/App.js
+++ ui-todo/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import login from './pages/login';
 import signup from './pages/signup';
+import home from './pages/home';
 
 function App() {
   return (
@@ -10,8 +11,9 @@ function App() {
       <Router>
         <div>
           <Switch>
-            <Route exact path='/login' component={login}/>
-            <Route exact path='/signup' component={signup}/>
+            <Route exact path='/login' component={login} />
+            <Route exact path='/signup' component={signup} />
+            <Route exact path='/' component={home} />
           </Switch>
         </div>
       </Router>
